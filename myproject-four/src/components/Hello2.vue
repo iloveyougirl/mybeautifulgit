a083b9a7aff8daef797649bfc906231db46ae39f
PC201610101509\Administrator <392562208@qq.com>
2016-11-02 10:54:01 +0800

    首次使用vue-router

---
+++ myproject-four/src/components/Hello2.vue
@@ -0,0 +1,16 @@
+<template id="grid-template">
+  <div class='content'>
+    <p>张树人是个大傻逼</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+  .content{
+    margin-left:192px
+  }
+</style>
